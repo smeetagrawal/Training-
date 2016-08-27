2d071a2d5e539039c55152007ebb206eff6730e5
Agrawals MHA <Agrawals MHA>
2016-08-28 02:47:28 +0530

    name

--- h.js
+++ h.js
@@ -4,13 +4,38 @@ var express = require ('express');
  app.get('/',function (req,res)
   {
 
-        res.send("hello world");
+        console.log(req.query.name);
 
+        res.send(`
+          <html>
+          <body>
+
+          <form action = '/home'  method = get />
+          enter name : <input type = "text" name = "name">
+
+          <input type = "submit" value = "submit" >
+
+
+          </body>
+          </html>
+
+
+
+
+          `)
+
+ app.get('/home',function (req,res)
+  {
+
+         res.send("hello world" + req.query.name );
 
  })
 
 
+ });
+
  app.listen(3000,function ()
   {
             console.log("hello world");
+
  })
