6c80c282f9a9f928de2e94eae7093933f037fc53
Agrawals MHA <Agrawals MHA>
2016-08-28 01:05:41 +0530

    using express

---
+++ h.js
@@ -0,0 +1,16 @@
+var express = require ('express');
+ var app = express();
+
+ app.get('/',function (req,res)
+  {
+
+        res.send("hello world");
+
+
+ })
+
+
+ app.listen(3000,function ()
+  {
+            console.log("hello world");
+ })
